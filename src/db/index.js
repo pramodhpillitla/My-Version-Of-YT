93ac61dd13b9a2437d0281dad793aad575d6881f
Pramodh Pillitla <pramodhpillitla@gmail.com>
2025-08-28 23:56:21 +0530

    db connection with mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+export const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    } catch (error) {
+        console.log("MONGO DB CONNECTION FAILED (ERROR) :",error);
+    }
+}
+
