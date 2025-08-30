38460cf2b6df5c41422bb6c2eab170a97213d7b6
Pramodh Pillitla <pramodhpillitla@gmail.com>
2025-08-30 15:13:50 +0530

    done with the set up of cloudinary and multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,10 @@
 class ApiError extends Error{
-    constructor(status,message ="something went wrong", errors, stack){
+    constructor(status,message ="something went wrong", errors=[], stack=""){
         super(message);
         this.status = status;
         this.errors = errors;
+        this.message = message;
+        this.success = false;
         if(stack){
             this.stack = stack;
         }else{
